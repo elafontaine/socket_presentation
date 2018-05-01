c7be94651b91afb3c5015329eb84159dfb584963
elafontaine <eric.lafontaine1@gmail.com>
2018-05-01 15:32:01 -0400

    Apply pep8 on client file

--- stuff_for_python_socket/Walkthrough/client.py
+++ stuff_for_python_socket/Walkthrough/client.py
@@ -2,7 +2,7 @@ import socket
 
 client_socket = socket.socket()
 
-client_socket.connect(("localhost",15000)) # blocking call, if it's to succeed
+client_socket.connect(("localhost", 15000))  # blocking call, if it's to succeed
 
 client_socket.send(b"those are the bytes you're looking for!")
 
