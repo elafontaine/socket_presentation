06113fd3c3eec257b45951cc26471ff0bbf1af0f
elafontaine <eric.lafontaine1@gmail.com>
2018-05-17 21:46:38 -0400

    add cygwin backgroun color start-up script

---
+++ start_cyg.bat
@@ -0,0 +1,5 @@
+
+C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico  -o BackgroundColour=0,0,64 -
+C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico  -o BackgroundColour=0,0,128 -
+C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico  -o BackgroundColour=0,64,0 -
+C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico  -o BackgroundColour=64,0,0 -
